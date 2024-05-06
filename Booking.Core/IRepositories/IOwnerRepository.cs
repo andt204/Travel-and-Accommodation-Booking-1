8694f2c3fb8e2ee4c718c899dc6e6b5b5f2ae6a0
XuanSonHoang <hson512475@gmail.com>
2024-05-06 17:11:22 +0700

    dic: add owner repository

---
+++ Booking.Core/IRepositories/IOwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookingHotel.Core.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingHotel.Core.IRepositories
+{
+    public interface IOwnerRepository
+    {
+      
+    }
+}
