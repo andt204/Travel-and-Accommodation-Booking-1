ee2b9d5db09b8ff0650b12f7d9685f8514f25cff
XuanSonHoang <hson512475@gmail.com>
2024-05-09 15:04:55 +0700

    direc: add model image, dto and response

---
+++ Booking.Core/Models/Domain/Image.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingHotel.Core.Models.Domain
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        [NotMapped]
+       
+        public IFormFile File { get; set; }
+        
+        public string FileName{ get; set; }
+    
+        public string FileDescription { get; set; }
+        public string FileExtension { get; set; }
+        public long FileSize { get; set; }
+        public string FilePath { get; set; }
+    }
+}
